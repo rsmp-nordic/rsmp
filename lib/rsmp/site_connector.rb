509e39fdff957438644ad0be7d8ef2b2cdd09fd6
Emil Tin <zf0f@tmf.kk.dk>
2019-10-22 12:50:36 +0200

    remove duplicated aggregated status methods

--- lib/rsmp/site_connector.rb
+++ lib/rsmp/site_connector.rb
@@ -68,14 +68,6 @@ module RSMP
       @version_determined = true
     end
 
-    def validate_aggregated_status  message, se
-      unless se && se.is_a?(Array) && se.size == 8
-        reason = 
-        dont_acknowledge message, "Received", "invalid AggregatedStatus, 'se' must be an Array of size 8"
-        raise InvalidMessage
-      end
-    end
-
     def set_aggregated_status se
       keys = [ :local_control,
                :communication_distruption,
@@ -103,22 +95,6 @@ module RSMP
         "se"=>@aggregated_status_bools
       })
       send message
-    end
-
-    def process_aggregated_status message
-      se = message.attribute("se")
-      validate_aggregated_status(message,se) == false
-      on = set_aggregated_status se
-      log "Received #{message.type} status [#{on.join(', ')}]", message
-      acknowledge message
-    end
-
-    def process_alarm message
-      alarm_code = message.attribute("aCId")
-      asp = message.attribute("aSp")
-      status = ["ack","aS","sS"].map { |key| message.attribute(key) }.join(',')
-      log "Received #{message.type}, #{alarm_code} #{asp} [#{status}]", message
-      acknowledge message
     end
 
     def process_command_request message
