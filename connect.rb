83a482741268f1d410620f5e9ecf23e176f65409
Emil Tin <zf0f@tmf.kk.dk>
2022-02-08 16:01:48 +0100

    cleanup

--- connect.rb
+++ connect.rb
@@ -3,25 +3,19 @@ require 'async/io'
 
 # client
 client_thread = Thread.new do
-
-  endpoint = Async::IO::Endpoint.tcp('0.0.0.0', 13111)
-
   Async do |task|
     timeout = 10
-
     task.async do |connect_task|
       endpoint = Async::IO::Endpoint.tcp('localhost', 12111)
       loop do
         puts "client: trying to connect to server"
         endpoint.connect
         puts 'client: connected to server'
-        exit
       rescue StandardError => e
         puts "client: error while connecting: #{e.inspect}"
         connect_task.sleep 1
       end
     end
-
     task.async do |cancel_task|
       cancel_task.sleep timeout
       puts "client: could not connect within #{timeout} sec - failure"
@@ -29,7 +23,6 @@ client_thread = Thread.new do
     end
   end
 end
-
 
 server_thread = Thread.new do
   timeout = 10
