074efca8fba5bb1a5b14464f2d56968bd2355f9e
Emil Tin <zf0f@tmf.kk.dk>
2022-02-08 16:17:04 +0100

    don't use async for client

--- connect.rb
+++ connect.rb
@@ -3,25 +3,14 @@ require 'async/io'
 
 # client
 client_thread = Thread.new do
-  Async do |task|
-    timeout = 20
-    task.async do |connect_task|
-      endpoint = Async::IO::Endpoint.tcp('localhost', 12111)
-      loop do
-        puts "client: trying to connect to server"
-        endpoint.connect
-        puts 'client: connected to server'
-        break
-      rescue StandardError => e
-        puts "client: error while connecting: #{e.inspect}"
-        connect_task.sleep 1
-      end
-    end
-    task.async do |cancel_task|
-      cancel_task.sleep timeout
-      puts "client: could not connect within #{timeout} sec - failure"
-      exit 1
-    end
+  5.times do
+    puts "client: trying to connect to server"
+    socket = TCPSocket.new 'localhost', 12111
+    puts "client: connected to server"
+    break
+  rescue StandardError => e
+    puts "client: error while connecting: #{e.inspect}"
+    sleep 1
   end
 end
 
