fd91ab8e98e06048f78243edc4b27f19bc809510
Emil Tin <zf0f@tmf.kk.dk>
2022-02-08 14:53:13 +0100

    async in two threads

--- connect.rb
+++ connect.rb
@@ -1,29 +1,52 @@
-require 'socket'
+require 'async'
+require 'async/io'
 
 # client
 client_thread = Thread.new do
-  5.times do
-    puts "client: connecting"
-    socket = TCPSocket.new 'localhost', 13111
-    puts "client: connected"
-    break
-  rescue StandardError => e
-    puts "couldn't not connect: #{e.inspect}"
-    sleep 1
+  Async do |task|
+    timeout = 10
+
+    task.async do |connect_task|
+      endpoint = Async::IO::Endpoint.tcp('127.0.0.1', 13111)
+      loop do
+        puts "client: trying to connect to server"
+        endpoint.connect
+        puts 'client: connected to server'
+        exit
+      rescue StandardError => e
+        puts "client: error while connecting: #{e.inspect}"
+        connect_task.sleep 1
+      end
+    end
+
+    task.async do |cancel_task|
+      cancel_task.sleep timeout
+      puts "client: could not connect within #{timeout} sec - failure"
+      exit 1
+    end
   end
 end
 
-# server
+
 server_thread = Thread.new do
-  puts "server: delay before starting"
-  sleep 3
-  server = TCPServer.new 13111
-  puts "server: waiting for client"
-  client = server.accept 
-  puts "server: client connected"
+  Async do |task|
+    timeout = 10
+
+    endpoint = Async::IO::Endpoint.tcp('0.0.0.0', 13111)
+    puts 'server: waiting for client to connect'
+    tasks = endpoint.accept do |socket|  # creates async tasks
+      puts "server: client connected - success"
+      exit
+    end
+
+#    task.async do |cancel_task|
+#      cancel_task.sleep timeout
+#      puts "client did not connect within #{timeout} sec"
+#      exit 1
+#    end
+  end
 end
 
-server_thread.join
-client_thread.join
 
-puts "done"
+client_thread.join
+server_thread.join
