b189a627f1ac2fa5a78f216cea1af41d830770ae
Emil Tin <emil@tin.dk>
2022-02-10 21:50:34 +0100

    no need to manually stop a transient task

--- spec/support/async_helper.rb
+++ spec/support/async_helper.rb
@@ -1,11 +1,13 @@
 require 'async'
 
+# Run block inside an Async reactor.
+# Catch errors and just return then, and use result() reo re-raise then outside the reactor,
+# to avoid Async printing errors, which interferes with rspec output.
+# Use a transient task, to ensure that any subtask are terminated as soon as the main task completes
 def async_context &block
 	Async transient: true do |task|
 		yield task
 	rescue StandardError => e
 		e
-	ensure
-		task.stop later: true
 	end.result
 end
