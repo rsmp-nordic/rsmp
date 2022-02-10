28c2804771f2022513df1ae737ecce47a444c0b0
Emil Tin <emil@tin.dk>
2022-02-10 21:56:13 +0100

    fix typos

--- spec/support/async_helper.rb
+++ spec/support/async_helper.rb
@@ -1,9 +1,9 @@
 require 'async'
 
 # Run block inside an Async reactor.
-# Catch errors and just return then, and use result() reo re-raise then outside the reactor,
+# Catch errors and return them, then use result() to re-raise them outside the reactor.
 # to avoid Async printing errors, which interferes with rspec output.
-# Use a transient task, to ensure that any subtask are terminated as soon as the main task completes
+# We use a transient task, to ensure that any subtask are terminated as soon as the main task completes.
 def async_context &block
 	Async transient: true do |task|
 		yield task
