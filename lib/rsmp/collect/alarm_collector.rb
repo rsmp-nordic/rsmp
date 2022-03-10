5e6d3bdc55aaef094ffb155bee032df918e0c1f0
Emil Tin <zf0f@tmf.kk.dk>
2022-03-10 09:43:08 +0100

    improve AlarmCollector to enable filtering on all attributes

--- lib/rsmp/collect/alarm_collector.rb
+++ lib/rsmp/collect/alarm_collector.rb
@@ -1,8 +1,8 @@
 module RSMP
   # Class for waiting for specific command responses
   class AlarmCollector < Collector
-    def initialize proxy, want, options={}
-      @want = want
+    def initialize proxy,options={}
+      @query = options[:query] || {}
       super proxy, options.merge(
         type: 'Alarm',
         title:'alarm'
@@ -11,8 +11,23 @@ module RSMP
 
     def type_match? message
       return false if super(message) == false
-      [:aCId, :aSp, :ack, :aS, :sS, :cat, :pri].each do |key|
-        return false if @want[key] && @want[key] != message.attribute(key.to_s)
+
+      # match fixed attributes
+      %w{aCId aSp ack aS sS cat pri}.each do |key|
+        return false if @query[key] && @query[key] != message.attribute(key)
+      end
+
+      # match rvs items
+      if @query['rvs']
+        query_rvs = @query['rvs']
+        message_rvs = message.attributes['rvs']
+        return false unless message_rvs
+        return false unless query_rvs.all? do |query_item|
+          return false unless message_rvs.any? do |message_item|
+            next message_item['n'] == query_item['n'] && message_item['v'] == query_item['v']
+          end
+          next true
+        end
       end
       true
     end
--- spec/alarm_collector_spec.rb
+++ spec/alarm_collector_spec.rb
@@ -1,13 +1,30 @@
 RSpec.describe RSMP::AlarmCollector do
   let(:timeout) { 0.001 }
-  let(:alarm) { RSMP::Alarm.new(
+  let(:right) { RSMP::Alarm.new(
       'aCId' => 'A0302',
       'aSp' => 'Issue',
-      'ack' => 'notAcknowledged',
+      'ack' => 'Acknowledged',
       'aS' => 'Active',
       'sS' => 'notSuspended',
       'aTs' => Time.now - 60,
       'cat' => 'D',
+      'pri' => '1',
+      'rvs' => [
+        {
+          'n' => 'color',
+          'v' => 'green'
+        }
+      ]
+    )
+  }
+  let(:wrong) { RSMP::Alarm.new(
+      'aCId' => 'A0303',
+      'aSp' => 'Suspend',
+      'ack' => 'notAcknowledged',
+      'aS' => 'inActive',
+      'sS' => 'suspended',
+      'aTs' => Time.now - 60,
+      'cat' => 'T',
       'pri' => '2',
       'rvs' => [
         {
@@ -22,45 +39,17 @@ RSpec.describe RSMP::AlarmCollector do
     it 'gets any alarm' do
       RSMP::SiteProxyStub.async do |task,proxy|
         collect_task = task.async do
-          collector = RSMP::AlarmCollector.new proxy, {}, num: 1, timeout: timeout
+          collector = RSMP::AlarmCollector.new proxy, num: 2, timeout: timeout
           result = collector.collect
 
           expect(result).to eq(:ok)
           expect(collector.messages).to be_an(Array)
-          expect(collector.messages.size).to eq(1)
-          expect(collector.messages.first).to be_an(RSMP::Alarm)
+          expect(collector.messages.size).to eq(2)
+          expect(collector.messages.first).to be(right)
+          expect(collector.messages.last).to be(wrong)
         end
-        proxy.notify alarm
-        collect_task.wait
-      end
-    end
-
-    it 'filters out wrong alarm' do
-      RSMP::SiteProxyStub.async do |task,proxy|
-        collect_task = task.async do
-          collector = RSMP::AlarmCollector.new proxy, {aCId: 'A0303'}, num: 1, timeout: timeout
-          result = collector.collect
-
-          expect(result).to eq(:timeout)
-        end
-        proxy.notify alarm
-        collect_task.wait
-      end
-    end
-
-    it 'gets correct alarm' do
-      RSMP::SiteProxyStub.async do |task,proxy|
-        collect_task = task.async do
-          collector = RSMP::AlarmCollector.new proxy, {aCId: 'A0302'}, num: 1, timeout: timeout
-          result = collector.collect
-
-          expect(result).to eq(:ok)
-          expect(collector.messages).to be_an(Array)
-          expect(collector.messages.size).to eq(1)
-          expect(collector.messages.first).to be_an(RSMP::Alarm)
-          expect(collector.messages.first.attribute('aCId')).to eq('A0302')
-        end
-        proxy.notify alarm
+        proxy.notify right
+        proxy.notify wrong
         collect_task.wait
       end
     end
@@ -68,7 +57,7 @@ RSpec.describe RSMP::AlarmCollector do
     it 'times out' do
       RSMP::SiteProxyStub.async do |task,proxy|
         collect_task = task.async do
-          collector = RSMP::AlarmCollector.new proxy, {}, num: 1, timeout: timeout
+          collector = RSMP::AlarmCollector.new proxy, num: 1, timeout: timeout
           result = collector.collect
 
           expect(result).to eq(:timeout)
@@ -76,6 +65,146 @@ RSpec.describe RSMP::AlarmCollector do
         collect_task.wait
       end
     end
-  end
 
+    it 'matches aCId' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'aCId' => 'A0302'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches aSp' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'aSp' => 'Issue'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches ack' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'ack' => 'Acknowledged'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches As' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'aS' => 'Active'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches sS' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'sS' => 'notSuspended'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches cat' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'cat' => 'D'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+    it 'matches pri' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          collector = RSMP::AlarmCollector.new proxy, query: {'pri' => '1'}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+
+    it 'matches rvs' do
+      RSMP::SiteProxyStub.async do |task,proxy|
+        collect_task = task.async do
+          rvs = [
+            {'n' => 'color', 'v' => 'green'}
+          ]
+          collector = RSMP::AlarmCollector.new proxy, query: {'rvs' => rvs}, num: 1, timeout: timeout
+          result = collector.collect
+
+          expect(result).to eq(:ok)
+          expect(collector.messages).to be_an(Array)
+          expect(collector.messages.size).to eq(1)
+          expect(collector.messages.first).to eq(right)
+        end
+        proxy.notify wrong
+        proxy.notify right
+        collect_task.wait
+      end
+    end
+
+  end
 end
