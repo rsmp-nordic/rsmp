fd015c16f6d2757e07955752180cc107a3100d06
Emil Tin <emil@tin.dk>
2023-06-09 15:26:43 +0200

    handle IOError, Errno::EBADF, EOFError

--- spec/alarm_collector_spec.rb
+++ spec/alarm_collector_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe RSMP::AlarmCollector do
-  let(:timeout) { 0.001 }
+  let(:timeout) { 0.01 }
   let(:right) { RSMP::Alarm.new(
       'cId' => 'DL1',
       'aCId' => 'A0302',
--- spec/connect_spec.rb
+++ spec/connect_spec.rb
@@ -88,6 +88,8 @@ RSpec.describe 'Connecting' do
 
 			expect(site_proxy.state).to eq(:ready)
 			expect(supervisor_proxy.state).to eq(:ready)
+
+			task.stop
 		end
 	end
 end
