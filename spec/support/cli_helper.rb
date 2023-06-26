d6340e723fea974a4a9a350b2fb0f3406aa550e6
Emil Tin <emil@tin.dk>
2023-06-26 12:43:48 +0200

    prepend cmd.exe when running cli on windows

--- spec/support/cli_helper.rb
+++ spec/support/cli_helper.rb
@@ -1,6 +1,7 @@
 require 'rsmp/cli'
 
 def run_cli(cmd, exit_timeout: 5)
+  cmd.unshift("cmd.exe", "/c") if ChildProcess.windows?
   run_command_and_stop(cmd, exit_timeout: exit_timeout, fail_on_error: false)
 end
 
