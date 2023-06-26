b18d6f8450fdc49d8061c60f71ad0530943dd3bc
Emil Tin <emil@tin.dk>
2023-06-26 15:51:20 +0200

    try cmd.exe /c

--- spec/support/cli_helper.rb
+++ spec/support/cli_helper.rb
@@ -1,7 +1,7 @@
 require 'rsmp/cli'
 
 def run_cli(cmd, exit_timeout: 5)
-  cmd.prepend("ruby -S") if Gem.win_platform?
+  cmd.prepend("cmd.exe /c ") if Gem.win_platform?
   run_command_and_stop(cmd, exit_timeout: exit_timeout, fail_on_error: false)
 end
 
