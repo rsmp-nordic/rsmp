b25e76eb1035f827ac6aa6def207edd1342ef689
Emil Tin <emil@tin.dk>
2023-06-26 21:02:21 +0200

    simplify

--- spec/cli/cli_help_spec.rb
+++ spec/cli/cli_help_spec.rb
@@ -1,18 +1,4 @@
-RSpec.describe 'CLI rsmp help', :type => :aruba do
-  describe 'with no options' do
-    it 'prints help' do
-      run_cli 'rsmp help'
-      expect_cli_output /Commands:/
-    end
-  end
-
-  describe 'site' do
-    it 'prints site help' do
-      run_cli 'rsmp help site'
-      expect_cli_output /Usage:/
-    end
-  end
-
+RSpec.describe 'aruba windows', :type => :aruba do
   it 'can run command and stop' do
     run_command_and_stop('rspec --help', exit_timeout: 1, fail_on_error: false)
   end
