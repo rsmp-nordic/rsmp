5c554c196318d2688fb735db2a02e3371416bb0d
Emil Tin <zf0f@tmf.kk.dk>
2023-08-10 10:44:41 +0200

    reduce

--- async.rb
+++ async.rb
@@ -1,5 +1,6 @@
 require 'async'
 
+puts "Example 1"
 error = nil
 Async do |task|
   original = $stdout.clone
@@ -18,3 +19,26 @@ ensure
   STDOUT.reopen original if original
   raise error if error
 end
+
+
+puts
+
+puts "Example 2"
+error = nil
+Async do |task|
+  original = $stdout.clone
+  input, output = IO.pipe
+  STDOUT.reopen(output)
+  task.async do
+    task.with_timeout(1) do
+      line = input.gets
+      STDERR.puts "stdout: #{line}"
+    end
+  end
+rescue StandardError => e
+  error = e
+ensure
+  STDOUT.reopen original if original
+  raise error if error
+end
+
