aa16ba91fa180b69ceef008c63dbe4ebd985b0ae
Emil Tin <zf0f@tmf.kk.dk>
2023-08-10 09:22:26 +0200

    try expect_stdout helper

--- async.rb
+++ async.rb
@@ -1,9 +1,13 @@
 require 'async'
+require_relative 'spec/support/cli_helper.rb'
+
 puts 'starting...'
 Async do |task|
-  task.with_timeout(0.1) do
-    puts 'reaading with timeout..'
-    STDIN.gets
+  expect_stdout( 'OK') do
+    puts 'before'
+    puts 'OK'
+    puts 'after'
+    #RSMP::CLI.new.invoke('site')
   end
 end
 puts 'done'
