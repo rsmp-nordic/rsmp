99ed7ad2dc1dffbbeabcea23cb89ab71be915f85
Emil Tin <zf0f@tmf.kk.dk>
2023-08-31 15:24:27 +0200

    add state

--- investigate/animals.rb
+++ investigate/animals.rb
@@ -4,9 +4,38 @@ require_relative 'app'
 
 SPEED = 2
 
+class Worker
+  module State
+    attr_reader :state
+
+    def initialize node
+      super
+      @state = initial_state
+    end
+
+    def initial_state
+      ''
+    end
+
+    def log str
+      return if str == @state
+      was = @state
+      @state = str
+      super( "#{was.to_s.ljust(7)} => #{str}" )
+    end
+  end
+end    
+
 # Animal
 class Animal < Worker
+  include State
+
+  def initial_state
+    'zzz'
+  end
+
   def run
+    sleep 2*rand(0..10) * 1.0/SPEED
     log 'wake'
     loop do
       sleep rand(0..10) * 1.0/SPEED
@@ -16,7 +45,13 @@ class Animal < Worker
   end
 
   def party
-    log 'jump' if rand(5).zero?
+    if rand(3).zero?
+      if @state == 'zzz'
+        log 'mmm'
+      else
+        log 'jump'
+      end
+    end
   end
 
   def stop
@@ -25,11 +60,18 @@ class Animal < Worker
 
   def failed(error)
     log error
+    sleep rand(0..10) * 1.0/SPEED
   end
 end
 
 # Place
 class Place < Worker
+  include State
+
+  def initial_state
+    'closed'
+  end
+
   def run
     log 'open'
     loop do
@@ -48,5 +90,6 @@ class Place < Worker
 
   def failed(error)
     log error
+    sleep rand(0..10) * 1.0/SPEED
   end
 end
--- investigate/programmatic.rb
+++ investigate/programmatic.rb
@@ -5,16 +5,6 @@ require_relative 'animals'
 app = App.new
 zoo = Node.new(id: :zoo1, supervisor: app, worker_class: Place, strategy: :all_for_one)
 Node.new(id: :rhino1, supervisor: zoo, worker_class: Animal, strategy: :all_for_one)
-Node.new(id: :tiger2, supervisor: zoo, worker_class: Animal, strategy: :rest_for_one)
-Node.new(id: :monkey3, supervisor: zoo, worker_class: Animal, strategy: :one_for_one)
-farm = Node.new(id: :farm2, supervisor: app, worker_class: Place, strategy: :rest_for_one)
-Node.new(id: :horse1, supervisor: farm, worker_class: Animal, strategy: :all_for_one)
-Node.new(id: :cow2, supervisor: farm, worker_class: Animal, strategy: :rest_for_one)
-Node.new(id: :goat3, supervisor: farm, worker_class: Animal, strategy: :one_for_one)
-house = Node.new(id: :house3, supervisor: app, worker_class: Place, strategy: :one_for_one)
-Node.new(id: :dog1, supervisor: house, worker_class: Animal, strategy: :all_for_one)
-Node.new(id: :cat2, supervisor: house, worker_class: Animal, strategy: :rest_for_one)
-Node.new(id: :hamster3, supervisor: house, worker_class: Animal, strategy: :one_for_one)
 
 begin
   Async do
@@ -23,4 +13,3 @@ begin
 rescue Interrupt
   puts
 end
-pp app.hierarchy
