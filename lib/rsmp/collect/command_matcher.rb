17e5aacf599668ffba4cb49fca2bb1f4c36a8ebc
Emil Tin <emil@tin.dk>
2025-10-03 08:05:58 +0000

    Reduce command_matcher complexity

--- lib/rsmp/collect/command_matcher.rb
+++ lib/rsmp/collect/command_matcher.rb
@@ -1,17 +1,28 @@
 module RSMP
-  # Match a specific command responses
   class CommandMatcher < Matcher
-    # Match a return value item against a matcher
-    def match?(item)
+    def match_code?(item)
       return nil if @want['cCI'] && @want['cCI'] != item['cCI']
       return nil if @want['n'] && @want['n'] != item['n']
 
+      true
+    end
+
+    def match_value?(item)
+      return true unless @want['v']
+
       if @want['v'].is_a? Regexp
-        return false if @want['v'] && item['v'] !~ @want['v']
-      elsif @want['v'] && item['v'] != @want['v']
-        return false
+        return false if item['v'] !~ @want['v']
+      else
+        return false if item['v'] != @want['v']
       end
       true
+    end
+
+    def match?(item)
+      code_match = match_code?(item)
+      return code_match if code_match.nil?
+
+      match_value?(item)
     end
   end
 end
