0d16e10627104fa75d40a18a09d9a3a4d2d2e070
Emil Tin <emil@tin.dk>
2025-10-03 08:01:22 +0000

    Extract command request helper methods

--- lib/rsmp/proxy/supervisor/modules/commands.rb
+++ lib/rsmp/proxy/supervisor/modules/commands.rb
@@ -14,29 +14,31 @@ module RSMP
           sorted
         end
 
-        def process_command_request(message)
-          component_id = message.attributes['cId']
-
-          rvs = message.attributes['arg'].map do |item|
+        def build_command_rvs(args)
+          args.map do |item|
             item = item.dup.merge('age' => 'recent')
             item.delete 'cO'
             item
           end
+        end
 
-          begin
-            component = @site.find_component component_id
-            commands = simplify_command_requests message.attributes['arg']
-            commands.each_pair do |command_code, arg|
-              component.handle_command command_code, arg
-            end
-            log "Received #{message.type}", message: message, level: :log
-          rescue UnknownComponent
-            log "Received #{message.type} with unknown component id '#{component_id}' and cannot infer type",
-                message: message, level: :warning
-            rvs.map do |item|
-              item['age'] = 'undefined'
-            end
+        def execute_commands(message, component_id, rvs)
+          component = @site.find_component component_id
+          commands = simplify_command_requests message.attributes['arg']
+          commands.each_pair do |command_code, arg|
+            component.handle_command command_code, arg
           end
+          log "Received #{message.type}", message: message, level: :log
+        rescue UnknownComponent
+          log "Received #{message.type} with unknown component id '#{component_id}' and cannot infer type",
+              message: message, level: :warning
+          rvs.map { |item| item['age'] = 'undefined' }
+        end
+
+        def process_command_request(message)
+          component_id = message.attributes['cId']
+          rvs = build_command_rvs(message.attributes['arg'])
+          execute_commands(message, component_id, rvs)
 
           response = CommandResponse.new({
                                            'cId' => component_id,
