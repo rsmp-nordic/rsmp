2c1f340cf26b8c39d1f14d08cae79a8327c76ca1
Emil Tin <emil@tin.dk>
2026-04-14 14:23:16 +0200

    add StatusList

---
+++ lib/rsmp/status_list.rb
@@ -0,0 +1,42 @@
+module RSMP
+  # Represents an RSMP status list and converts between the two common formats:
+  #
+  # Compact Hash (symbol or string keys):
+  #   { S0014: [:status, :source] }
+  #   { 'S0014' => ['status', 'source'] }
+  #
+  # Raw wire Array (used in RSMP messages):
+  #   [{ 'sCI' => 'S0014', 'n' => 'status' }, { 'sCI' => 'S0014', 'n' => 'source' }]
+  class StatusList
+    include Enumerable
+
+    def initialize(input)
+      @list = case input
+              when Array
+                input
+              when Hash
+                input.flat_map do |code, names|
+                  names.map { |name| { 'sCI' => code.to_s, 'n' => name.to_s } }
+                end
+              else
+                raise ArgumentError, "StatusList requires an Array or Hash, got #{input.class}"
+              end
+    end
+
+    def each(&)
+      @list.each(&)
+    end
+
+    def to_a
+      @list
+    end
+
+    def to_h
+      @list.each_with_object({}) do |item, hash|
+        code = item['sCI']
+        name = item['n']
+        (hash[code] ||= []) << name
+      end
+    end
+  end
+end
